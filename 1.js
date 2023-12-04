7e9d87947866bec3130a06405570cfc9be4a5712
Your Name <you@example.com>
2023-12-04 17:48:28 +0200

    done

--- 1.js
+++ 1.js
@@ -2,16 +2,11 @@
 
 let input = require("./input1")
 
-let input2 = `
-383nineeight9eightjfdhmjfrj
-
-`
-
-let rows = input2.split("\n")
+let rows = input.split("\n")
 
 let res = 0
 
-function replaceWords(row) {
+function calc(row) {
   let wordsToNumbers = {
     one: "1",
     two: "2",
@@ -22,25 +17,21 @@ function replaceWords(row) {
     seven: "7",
     eight: "8",
     nine: "9",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
   }
-
-  console.log("row", row)
-  //   let regex = new RegExp(Object.keys(wordsToNumbers).join("|"), "g")
-  //   let matches = row.match(regex)
-  //   if (matches) {
-  //   console.log(`Слово  знайдено ${matches} `)
-  //     let firstWord = matches[0]
-  //     let lastWord = matches[matches.length - 1]
-
-  //     row = row.replace(firstWord, wordsToNumbers[firstWord])
-  //     row = row.replace(lastWord, wordsToNumbers[lastWord])
-  //   } else {
-  //  //   console.log(`Слово не знайдено у ${row}`)
-  //   }
 
   let keys = Object.keys(wordsToNumbers)
   let matches = []
 
+  //finds occurrences in a string that overlap each other
   for (let i = 0; i < row.length; i++) {
     for (let key of keys) {
       if (row.substring(i).startsWith(key)) {
@@ -49,39 +40,15 @@ function replaceWords(row) {
     }
   }
 
-  console.log(matches)
-
-  let firstWord = matches[0]
-  let lastWord = matches[matches.length - 1]
-
-  row = row.replace(firstWord, wordsToNumbers[firstWord])
-  row = row.replace(lastWord, wordsToNumbers[lastWord])
-
-  console.log("row", row)
-  return row
+  res =
+    res +
+    +(wordsToNumbers[matches[0]] + wordsToNumbers[matches[matches.length - 1]])
 }
 
-for (let rowInit of rows) {
-  let row = replaceWords(rowInit)
-
-  let firstNumber = ""
-  let lastNumber = ""
-
-  for (let char of row) {
-    if (!isNaN(+char)) {
-      firstNumber = char
-      break
-    }
+for (let row of rows) {
+  if (row.trim() !== "") {
+    calc(row)
   }
-
-  for (let i = row.length - 1; i >= 0; i--) {
-    if (!isNaN(+row[i])) {
-      lastNumber = row[i]
-      break
-    }
-  }
-  console.log(firstNumber, lastNumber)
-  res = res + +(firstNumber + lastNumber)
 }
 
 console.log(res)
